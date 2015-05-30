3a81be6e27e7674c594b3022a9e97862c412d4ad
jgnatch <nachojgutierrez@gmail.com>
2015-05-30 19:19:24 -0300

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  ListView,
+  View,
+} = React;
+
+var MOCK_RECIPES = [{title: "Risotto de Hongos"}, {title: "Chicken Parmesan"}]
+
+var recipesApp = React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows(MOCK_RECIPES),
+    };
+  },
+
+  render: function() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={ this.renderRecipeListItem }
+        style={styles.recipesList}
+      />
+    );
+  },
+
+  renderRecipeListItem: function(recipe) {
+    return (
+      <View style={styles.recipeListItem}>
+        <Text style={styles.recipeListItemText}>
+          { recipe.title }
+        </Text>
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ff505a',
+  },
+  recipesList: {
+    backgroundColor: "#ff505a"
+  },
+  recipeListItem: {
+    padding: 10,
+    paddingBottom: 20,
+    paddingTop: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ff7880"
+  },
+  recipeListItemText: {
+    fontSize: 20,
+    color: "#FFFFFF",
+    fontFamily: "Helvetica Neue",
+  }
+});
+
+AppRegistry.registerComponent('recipesApp', () => recipesApp);
