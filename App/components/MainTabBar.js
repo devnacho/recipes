b0f35fe2450d9635f470fd659c5186051e2860d5
jgnatch <nachojgutierrez@gmail.com>
2015-06-01 21:11:38 -0300

    code clean up

--- App/components/MainTabBar.js
+++ App/components/MainTabBar.js
@@ -21,23 +21,26 @@ var MainTabBar = React.createClass({
     };
   },
 
+  _pressTabItem: function(tabName) {
+    this.setState({
+      selectedTab: tabName
+    });
+  },
+
   render: function() {
     return (
       <TabBarIOS tintColor="black" barTintColor="white">
         <TabBarIOS.Item title="Recipe" selected={ this.state.selectedTab === 'recipesTab' }
           onPress={() => {
-            this.setState({
-              selectedTab: 'recipesTab',
-            });
+            this._pressTabItem('recipesTab');
           }}>
           <NavigatorComponent></NavigatorComponent>
         </TabBarIOS.Item>
 
         <TabBarIOS.Item title="Groceries" selected={ this.state.selectedTab === 'groceriesTab' }
-          onPress={() => {
-            this.setState({
-              selectedTab: 'groceriesTab',
-            });
+          onPress={
+            () => {
+            this._pressTabItem('groceriesTab');
           }}>
           <Hello></Hello>
         </TabBarIOS.Item>
