8f66be4ee7a713b22aca76484a1d22f1f3294e6a
jgnatch <nachojgutierrez@gmail.com>
2015-06-16 12:04:47 -0300

    basic ingredients

--- App/components/IngredientsList.js
+++ App/components/IngredientsList.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+
 var {
   AppRegistry,
   StyleSheet,
@@ -11,18 +12,42 @@ var {
 
 var IngredientsList = React.createClass({
 
+  getDefaultProps: function() {
+    return {
+      //Replace this with real props
+      ingredients: [{name: "Blue Cheese"}, {name: "Meat"}, {name: "Mustard"}],
+    };
+  },
+
+
 
   render: function() {
+    var ingredientRows = []
+    for (var i = 0; i < this.props.ingredients.length; i++) {
+      ingredientRows.push(this.renderIngredientListItem(this.props.ingredients[i]))
+    }
     return (
       <View>
-        <Text>
-          IngredientsList
-        </Text>
+        {ingredientRows}
+      </View>
+    );
+  },
+
+  renderIngredientListItem: function(ingredient) {
+    return (
+      <View style={styles.ingredientListItem}>
+        <Text>{ingredient.name}</Text>
       </View>
     );
   },
 });
 
+var styles = StyleSheet.create({
+
+  ingredientListItem: {
+  },
+
+});
 
 
 module.exports = IngredientsList;
